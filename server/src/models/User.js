e089d0128d4fe701028aa964e01c6311bc011d32
thatiannecarvalho <thatianne.cristina@hotmail.com>
2020-07-23 19:47:13 -0300

    :lipstick: handler name and email size

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, unique: true, required: true, trim: true },
-  email: { type: String, required: true, trim: true },
+  name: { type: String, unique: true, required: true, trim: true, maxlength: 50 },
+  email: { type: String, required: true, trim: true, maxlength: 50},
   friend: String
 })
 
